090ae7477d41a5abbae4f50a763dee77e955a3b4
Hailie Jade Juliano <Hailiejadejuliano@gmail.com>
2026-02-19 14:57:01 -0600

    setted up the log in page

--- code/script.js
+++ code/script.js
@@ -1,5 +1,6 @@
 // Wait for the animation to finish (e.g., 3000 milliseconds for a 3s animation)
+
 setTimeout(function() {
     document.querySelector('.splash-screen').style.display = 'none';
     document.querySelector('.main-content').style.display = 'block';
-}, 3100); // Adjust time to match your CSS animation duration
+}, 3100); // Adjust time to match your CSS animation duration*/
