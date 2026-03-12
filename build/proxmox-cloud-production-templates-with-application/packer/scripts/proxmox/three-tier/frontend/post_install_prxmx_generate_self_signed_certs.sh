9ebdd0bcbdbd0427fcb5e43de97e9f2585ac5f55
Hailie Jade Juliano <Hailiejadejuliano@gmail.com>
2026-03-12 00:42:15 -0500

    made changes to cert, ownership and create services script

--- build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_generate_self_signed_certs.sh
+++ build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_generate_self_signed_certs.sh
@@ -2,7 +2,6 @@
 
 # https://stackoverflow.com/questions/10175812/how-to-create-a-self-signed-certificate-with-openssl
 # https://ethitter.com/2016/05/generating-a-csr-with-san-at-the-command-line/
-sudo mkdir -p /home/nextjsuser
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /home/nextjsuser/signed.key -out /home/nextjsuser/signed.crt -subj "/C=US/ST=IL/L=Chicago/O=IIT/OU=itm/CN=iit.edu"
 #installs nginx
 sudo apt update 
--- build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_move_application_files_for_flask_app.sh
+++ build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_move_application_files_for_flask_app.sh
@@ -4,19 +4,19 @@ set -v
 
 # https://copilot.microsoft.com/shares/nis5fbJZkaup7K34PRKwK
 
-sudo mv /home/vagrant/team-00/code/python-flask/app.py /home/flaskuser/app.py
-sudo mv /home/vagrant/team-00/code/python-flask/.env /home/flaskuser/.env
-sudo mv /home/vagrant/team-00/code/python-flask/static/ /home/flaskuser/static/
-sudo mv /home/vagrant/team-00/code/python-flask/templates/ /home/flaskuser/templates/
+sudo mv /home/vagrant/sp2026-team05/code/python-flask/app.py /home/nextjsuser/app.py
+sudo mv /home/vagrant/sp2026-team05/code/python-flask/.env /home/nextjsuser/.env
+sudo mv /home/vagrant/sp2026-team05/code/python-flask/static/ /home/nextjsuser/static/
+sudo mv /home/vagrant/sp2026-team05/code/python-flask/templates/ /home/nextjsuser/templates/
 
 # How to use an ENV variable in a sed command
 # https://askubuntu.com/questions/76808/how-do-i-use-variables-in-a-sed-command
-# sed -i "s/REPLACE/$APPVAULT_TOKEN/g" /home/flaskuser/.env
-sudo chown -R flaskuser:flaskuser /home/flaskuser/*
+# sed -i "s/REPLACE/$APPVAULT_TOKEN/g" /home/nextjsuser/.env
+sudo chown -R nextjsuser:nextjsuser /home/nextjsuser/*
 
 # Move the service file into /etc/systemd/system which is where user created
 # service files are placed by convention
 # Enable Flask App service to boot at start 
 # from /etc/systemd/system/flask-app.service
-sudo mv /home/vagrant/team-00/code/python-flask/flask-app.service /etc/systemd/system/flask-app.service
-sudo systemctl enable flask-app.service
+# sudo mv /home/vagrant/team-00/code/python-flask/flask-app.service /etc/systemd/system/flask-app.service
+# sudo systemctl enable flask-app.service
