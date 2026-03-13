fe473bcbac75ca049ded8b6f83c91ae233f8562a
Hailie Jade Juliano <Hailiejadejuliano@gmail.com>
2026-03-13 02:15:18 -0500

    added server.js to nextjs-project for server custom server

---
+++ code/nextjs-project/server.js
@@ -0,0 +1,14 @@
+import { createServer } from 'http'
+import next from 'next'
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res)
+  }).listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
+})
