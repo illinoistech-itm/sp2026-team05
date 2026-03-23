26eb22e70a5b6e78f0abf0ff4b531c1b2c5ed4d6
Hailie Jade Juliano <Hailiejadejuliano@gmail.com>
2026-03-23 11:00:59 -0500

    rerouted log in button to home page

--- code/nextjs-project/app/loginbutton.tsx
+++ code/nextjs-project/app/loginbutton.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react"
 export default function LoginButton() {
   return (
     <button
-      onClick={() => signIn("google", { callbackUrl: "/home/home.tsx" })}
+      onClick={() => signIn("google", { callbackUrl: "/home/page.tsx" })}
       className="bg-photoblue text-white text-sm underline rounded-sm h-9 px-9"
     >
       Log in with Google
