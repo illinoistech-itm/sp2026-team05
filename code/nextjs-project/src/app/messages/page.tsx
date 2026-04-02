93047b261c743bdc5943c3818e80c83de4ff8ba6
Hailie Jade Juliano <Hailiejadejuliano@gmail.com>
2026-04-02 18:23:04 -0500

    move hooks from server to client message.tsx

--- code/nextjs-project/src/app/messages/page.tsx
+++ code/nextjs-project/src/app/messages/page.tsx
@@ -1,11 +1,12 @@
-// app/messages/page.tsx
+import { Suspense } from "react";
 import MessagesClient from "./MessagesClient";
 
 export default function MessagesPage() {
   return (
     <div className="min-h-screen">
-      <h1 className="text-white font-mono text-2xl p-4">Messages</h1>
-      <MessagesClient />
+      <Suspense fallback={<div>Loading messages...</div>}>
+        <MessagesClient />
+      </Suspense>
     </div>
   );
 }
