036337a6a5de3d66f44c875d4c6c1629c801a31e
Hailie Jade Juliano <Hailiejadejuliano@gmail.com>
2026-04-11 14:04:33 -0500

    made more changes to fix google oath

--- build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_update_env_values_from_vault.sh
+++ build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_update_env_values_from_vault.sh
@@ -8,14 +8,23 @@ set -v
 # this is a hack to pass environment variables into the vm instances
 ###############################################################################
 
-sudo sed -i "s|FQDN=|FQDN=$FQDN|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|DBUSER=|DBUSER=$DBUSER|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|DBPASS=|DBPASS=$DBPASS|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|DATABASE=|DATABASE=$DATABASE|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|APP_SECRET=|APP_SECRET=$APP_SECRET|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|AUTH_GOOGLE_ID=|CLIENTID=$CLIENTID|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|AUTH_GOOGLE_SECRET=|CLIENTSECRET=$CLIENTSECRET|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|AUTH_TRUST_HOST=|AUTH_TRUST_HOST=$AUTH_TRUST_HOST|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|AUTH_URL=|AUTH_URL=$AUTH_URL|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|NEXTAUTH_SECRET=|NEXTAUTH_SECRET=$NEXTAUTH_SECRET|" /home/vagrant/nextjs-project/.env
-sudo sed -i "s|NEXTAUTH_URL=|NEXTAUTH_URL=$NEXTAUTH_URL|" /home/vagrant/nextjs-project/.env
+APP_SECRET_VALUE="${APP_SECRET:-$AUTH_SECRET}"
+CLIENT_ID_VALUE="${CLIENTID:-$AUTH_GOOGLE_ID}"
+CLIENT_SECRET_VALUE="${CLIENTSECRET:-$AUTH_GOOGLE_SECRET}"
+
+sudo sed -i "s|^FQDN=|FQDN=$FQDN|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^DBUSER=|DBUSER=$DBUSER|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^DBPASS=|DBPASS=$DBPASS|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^DATABASE=|DATABASE=$DATABASE|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^APP_SECRET=|APP_SECRET=$APP_SECRET_VALUE|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^CLIENTID=|CLIENTID=$CLIENT_ID_VALUE|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^CLIENTSECRET=|CLIENTSECRET=$CLIENT_SECRET_VALUE|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^NEXTAUTH_URL=|NEXTAUTH_URL=$NEXTAUTH_URL|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^NEXTAUTH_SECRET=|NEXTAUTH_SECRET=$NEXTAUTH_SECRET|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^AUTH_SECRET=|AUTH_SECRET=$AUTH_SECRET|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^AUTH_GOOGLE_ID=|AUTH_GOOGLE_ID=$AUTH_GOOGLE_ID|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^AUTH_GOOGLE_SECRET=|AUTH_GOOGLE_SECRET=$AUTH_GOOGLE_SECRET|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^GOOGLE_CLIENT_ID=|GOOGLE_CLIENT_ID=$CLIENT_ID_VALUE|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^GOOGLE_CLIENT_SECRET=|GOOGLE_CLIENT_SECRET=$CLIENT_SECRET_VALUE|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^AUTH_TRUST_HOST=|AUTH_TRUST_HOST=$AUTH_TRUST_HOST|" /home/vagrant/nextjs-project/.env
+sudo sed -i "s|^AUTH_URL=|AUTH_URL=$AUTH_URL|" /home/vagrant/nextjs-project/.env
