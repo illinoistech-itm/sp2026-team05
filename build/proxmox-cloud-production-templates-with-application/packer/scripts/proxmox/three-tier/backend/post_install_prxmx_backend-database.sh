ce33a027e093dc243bfd097fb37951108973f39a
Hailie Jade Juliano <Hailiejadejuliano@gmail.com>
2026-04-16 18:05:15 -0500

    updated database installation for mysql and added a check

--- build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/backend/post_install_prxmx_backend-database.sh
+++ build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/backend/post_install_prxmx_backend-database.sh
@@ -2,10 +2,21 @@
 
 # Install and prepare backend database
 echo "############ Installing My SQL Server #####################"
+# Add MySQL GPG key
+wget https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo apt-key add RPM-GPG-KEY-mysql-2022
+
+# Add repo manually (example for Ubuntu 22.04)
+echo "deb http://repo.mysql.com/apt/ubuntu/ jammy mysql-8.0" | sudo tee /etc/apt/sources.list.d/mysql.list
+
+# Install
 sudo apt update
-sudo apt install mysql-server-8.0
+sudo apt install -y mysql-server
 echo "############ Install of MySQL Server Complete #####################"
 # Required to have the mysql.service start at boot time
+dpkg -l | grep -i mysql
+dpkg -l | grep -i maria
+systemctl list-unit-files | grep -E 'mysql|maria'
 sudo systemctl enable mysql.service
 sudo systemctl start mysql.service
 sudo systemctl status mysql.service
