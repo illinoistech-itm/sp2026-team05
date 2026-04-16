f904223e6cbf8738f69c882d41a2da6f930afcfc
Hailie Jade Juliano <Hailiejadejuliano@gmail.com>
2026-04-16 18:32:27 -0500

    reverted back the backend database script to mariadb

--- build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/backend/post_install_prxmx_backend-database.sh
+++ build/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/backend/post_install_prxmx_backend-database.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
 # Install and prepare backend database
-echo "############ Installing My SQL Server #####################"
-# Add MySQL GPG key
-wget https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
-sudo apt-key add RPM-GPG-KEY-mysql-2022
-
-# Add repo manually (example for Ubuntu 22.04)
-echo "deb http://repo.mysql.com/apt/ubuntu/ jammy mysql-8.0" | sudo tee /etc/apt/sources.list.d/mysql.list
-
-# Install
+echo "############ Installing Maria DB Server #####################"
 sudo apt update
-sudo apt install -y mysql-server
-echo "############ Install of MySQL Server Complete #####################"
-# Required to have the mysql.service start at boot time
-dpkg -l | grep -i mysql
-dpkg -l | grep -i maria
-systemctl list-unit-files | grep -E 'mysql|maria'
-sudo systemctl enable mysql.service
-sudo systemctl start mysql.service
-sudo systemctl status mysql.service
-## Below uncomment for a testing
-# sudo mysql_secure_installation
+sudo apt install -y mariadb-server
+echo "############ Install of Maria DB Server Complete #####################"
+# Required to have the mariadb.service start at boot time
+sudo systemctl enable mariadb.service
+sudo systemctl start mariadb.service
 ## During the Terraform apply phase -- we will make some run time adjustments
 # to configure the database to listen on the meta-network interface only
